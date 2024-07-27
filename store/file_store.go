01df0dcae93595f0541e42a12778462957497b75
lsjhtang <lsjhtang@163.com>
2024-07-28 00:21:42 +0800

    store init

---
+++ store/file_store.go
@@ -0,0 +1,112 @@
+package store
+
+import (
+	"crypto/sha1"
+	"encoding/hex"
+	"io"
+	"os"
+	"path"
+	"strings"
+)
+
+func CASPathTransportFromFun(key string) PathKey {
+	hash := sha1.Sum([]byte(key))
+	hashStr := hex.EncodeToString(hash[:])
+
+	blockSize := 5
+	sliceLen := len(hashStr) / blockSize
+	paths := make([]string, sliceLen)
+
+	for i := 0; i < sliceLen; i++ {
+		from, to := i*blockSize, (i*blockSize)+blockSize
+		paths[i] = hashStr[from:to]
+	}
+	return PathKey{
+		PathName: strings.Join(paths, "/"),
+		Original: hashStr,
+	}
+}
+
+type PathKey struct {
+	PathName string
+	Original string
+}
+
+type PathTransportFromFun func(string) PathKey
+
+type Opts struct {
+	Root                 string
+	PathTransportFromFun PathTransportFromFun
+}
+
+type FileStore struct {
+	Opts
+}
+
+func NewFileStore(opts Opts) *FileStore {
+	if opts.PathTransportFromFun == nil {
+		opts.PathTransportFromFun = CASPathTransportFromFun
+	}
+	return &FileStore{Opts: opts}
+}
+
+func (s *FileStore) Write(key string, r io.Reader) error {
+	pathKey := s.PathTransportFromFun(key)
+	return s.writeStream(pathKey, r)
+}
+
+func (s *FileStore) writeStream(pathKey PathKey, r io.Reader) error {
+	err := os.MkdirAll(s.fullPathDir(pathKey), os.ModePerm)
+	fs, err := os.OpenFile(s.fullFileName(pathKey), os.O_CREATE|os.O_WRONLY, os.ModePerm)
+	if err != nil {
+		return err
+	}
+	defer fs.Close()
+
+	_, err = io.Copy(fs, r)
+	return err
+}
+
+func (s *FileStore) Has(key string) bool {
+	pathKey := s.PathTransportFromFun(key)
+	_, err := os.Stat(s.fullFileName(pathKey))
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func (s *FileStore) Remove(key string) error {
+	pathKey := s.PathTransportFromFun(key)
+	return os.Remove(s.fullFileName(pathKey))
+}
+
+func (s *FileStore) RemoveAll(key string) error {
+	pathKey := s.PathTransportFromFun(key)
+	firstPath := pathKey.PathName[:strings.Index(pathKey.PathName, "/")]
+	return os.RemoveAll(path.Join(s.Root, firstPath))
+}
+
+func (s *FileStore) Read(key string) (io.Reader, error) {
+	pathKey := s.PathTransportFromFun(key)
+	return s.readStream(pathKey)
+}
+
+func (s *FileStore) readStream(pathKey PathKey) (r io.Reader, err error) {
+	fs, err := os.OpenFile(s.fullFileName(pathKey), os.O_RDONLY, os.ModePerm)
+	if err != nil {
+		return fs, err
+	}
+	defer fs.Close()
+	_, err = io.Copy(fs, r)
+
+	return r, err
+}
+
+func (s *FileStore) fullPathDir(pathKey PathKey) string {
+	return path.Join(s.Root, pathKey.PathName)
+}
+
+func (s *FileStore) fullFileName(pathKey PathKey) string {
+	return path.Join(s.fullPathDir(pathKey), pathKey.Original)
+}
---
+++ store/file_store_test.go
@@ -0,0 +1,46 @@
+package store
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestCASPathTransportFromFun(t *testing.T) {
+	pathKey := CASPathTransportFromFun("lsjhtang")
+	pathName := "b2e03/95f49/af5ea/07c95/073a1/fbf57/18612/615d1"
+	original := "b2e0395f49af5ea07c95073a1fbf5718612615d1"
+	if pathKey.PathName != pathName {
+		t.Fatalf("want pathName: %s, got pathName:%s", pathName, pathKey.PathName)
+	}
+	if pathKey.Original != original {
+		t.Fatalf("want original: %s, got original:%s", original, pathKey.Original)
+	}
+}
+
+func TestStore(t *testing.T) {
+	opts := Opts{
+		Root:                 "root",
+		PathTransportFromFun: nil,
+	}
+	store := NewFileStore(opts)
+	key := "lsjhtang"
+	buf := bytes.NewBuffer([]byte(key))
+	err := store.Write(key, buf)
+	if err != nil {
+		t.Fatal(err)
+	}
+	bl := store.Has(key)
+	if !bl {
+		t.Fatalf("store does not have key %s", key)
+	}
+
+	err = store.RemoveAll(key)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	bl = store.Has(key)
+	if bl {
+		t.Fatalf("removell fail have key %s", key)
+	}
+}
